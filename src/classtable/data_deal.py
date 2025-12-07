338a692a66a97639ebd966260dc6fcca80c1fbfc
shunw <shunw.li@outlook.com>
2025-12-07 11:47:24 +0800

    Info list to df (#1)
    * change the strategy from dealing with df to dealing with class list.
    
    * remove previous function which is dealing with the df
    
    * realize the list sort by time and day in the main py
    
    * file struction changed

---
+++ src/classtable/data_deal.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import time
+from datetime import datetime
+
+class SingleSub:
+  def __init__(self, one_s:pd.Series):
+    self.start_time = one_s.start_time
+    self.class_name = one_s.class_name
+    self.class_loc = one_s.location
+    self.class_category = one_s.category
+    self.class_dis = one_s.home_dis
+    self.class_priority = one_s.priority
+    self.day = one_s.day
+
+    self.start_time_r = None
+  
+  def dict_form(self):
+    return (self.__dict__)
+
+  def __str__(self):
+    # return f'class:{self.class_name}; day:{self.day}; start_time:{self.start_time}; loc:{self.class_loc}; category:{self.class_category}'
+    return f'{self.__dict__}'
+
+
+
+class Subject:
+  def __init__(self):
+    pass
+
+  def inf_list_create(self):
+    loc_df = pd.read_csv('db/loc_inf.csv')
+    class_inf_df = pd.read_csv('db/class_inf.csv')
+    class_all_df = pd.read_csv('db/class_all.csv')
+    df = pd.merge(class_all_df,class_inf_df, on = 'class_id', how = 'left')
+    self.df = pd.merge(df, loc_df, on = 'loc_id', how = 'left')
+
+    self.inf_ls = list()
+    for ind in self.df.index:
+      self.inf_ls.append(SingleSub(self.df.loc[ind])) 
+      self.inf_ls[0].dict_form()
+      
+    return self.inf_ls
+    
+  
+  def _refine_new_time(self,time_str:str):
+    h, m = time_str.split(':')
+    if int(m) - 30 > 15: 
+      return f'{int(h)+1}:00'
+    elif (int(m) - 30 <= 15) and (int(m) - 30 > -15):
+      return f'{int(h)}:30'
+    elif int(m) - 30 <= -15:
+      return f'{int(h)}:00'
+    else:
+      return 'something wrong'
+    
+
+  def deal_time_slot(self):
+    '''
+    this is for the information list, this is to make the time into the block, like (11:25,11:40) -> 11:30; (11:05, 10:50)-> 11:00; 
+    
+    :param self: Description
+    '''
+    st = 'start_time'
+    # stn = 'start_time_r'
+ 
+    self.min_p, self.max_p = None, None
+    for i in self.inf_ls:
+      i.start_time_r = datetime.strptime(self._refine_new_time(i.start_time), '%H:%M').time()
+      if self.min_p is None: 
+        self.min_p = i.start_time_r
+      elif self.min_p > i.start_time_r:
+        self.min_p = i.start_time_r
+      
+      if self.max_p is None:
+        self.max_p = i.start_time_r
+      elif self.max_p < i.start_time_r:
+        self.max_p = i.start_time_r
+
+  def sort_inflist_for_table(self):
+    '''
+    purpose is to sort the information for the further usage. 
+
+    '''
+    # k_list = ['start_time_r', 'day']
+    self.inf_ls_new = []
+    
+    self.inf_ls_new = sorted(self.inf_ls, key = lambda x: (x.start_time_r, x.day))
+    return self.inf_ls_new
+
+  def convert_inflist_to_df(self, target_inflist:list[SingleSub]):
+    data = []
+    for i in target_inflist:
+      data.append(i.dict_form())
+    df = pd.DataFrame(data)
+    return df
+    # pass
+
+  def __str__(self):
+    pass
+
+class Timetable:
+  def __init__(self):
+    pass
+
+  def add_subject(self, day, period, subject):
+    if day not in self.schedule:
+        self.schedule[day] = {}
+        self.schedule[day][period] = subject
+
+  def get_subject(self, day, period):
+    return self.schedule[day][period]
+
+
+if __name__ == '__main__':
+  s = Subject()
+
+  s.inf_list_create()
+
+  s.deal_time_slot()
+  
+  inf_ls = s.sort_inflist_for_table()
+  df = s.convert_inflist_to_df(inf_ls)
+  print (df)
+  
+  
+  
--- src/classtable/out_table.py
+++ src/classtable/out_table.py
@@ -1,17 +1,19 @@
 import time
 import pandas as pd
 import numpy as np
+from classtable.data_deal import Subject, SingleSub
 
 def printGroup(group, keyHeader, keyMaxLen):
     whole = ''
     last_time = ''
     new_time_flag = True
+    start_time_r = 'start_time_r'
     for item in group:
         # print ('\r')
         w = ''
 
-        if (last_time == '') or (last_time != str(item['time'])):
-            last_time =  item['time']
+        if (last_time == '') or (last_time != str(item[start_time_r])):
+            last_time =  item[start_time_r]
             new_time_flag = True
         else:
             new_time_flag = False
@@ -25,7 +27,7 @@ def printGroup(group, keyHeader, keyMaxLen):
             else:
                 s = str(item[h]).center(itemLen, '-' if item[h] == '-' else ' ')
             
-            if not(new_time_flag) and (h == 'time'):
+            if not(new_time_flag) and (h == start_time_r):
                 s = str('').center(itemLen, '-' if item[h] == '-' else ' ')
 
             icon = '|'
@@ -64,7 +66,7 @@ class RefineTableData:
         self.keyMaxLen = get_max_width(table_data)
         self.table_data = table_data.copy()
     
-    def table_data_restru(self):
+    def table_data_restru_termform(self):
         # 占位项
         tag = {}
         for i,h in enumerate(self.keyHeader):
@@ -75,6 +77,24 @@ class RefineTableData:
         t = {i:i for i in self.keyHeader}
         self.table_data.insert(0,t)
         self.table_data.insert(0,tag)
+
+class RefineTable2csv:
+    '''
+    this is for the SingleSub in main.py only. 
+    main purpose is to make a table for the gym class view by time in vertical and day in horizontal
+    '''
+    def __init__(self, inf_ls:list[SingleSub]):
+        self.inf_ls = inf_ls
+
+    def get_original_df(self):
+        ls_data = []
+        for i in self.inf_ls:
+            ls_data.append(i.dict_form())
+        needed_head = ['start_time_r', 'day', 'class_name', 'class_loc']
+        self.df = pd.DataFrame(ls_data)[needed_head]
+        print (self.df)
+        return self.df
+
     
     
 def table_printout(table_data):
@@ -84,16 +104,38 @@ def table_printout(table_data):
     :param table_data: the data you want to printout as a table, and the format of the data is the df.to_dict('records')
     '''
     rd = RefineTableData(table_data)
-    rd.table_data_restru()
+    rd.table_data_restru_termform()
 
     # 打印后面的数据项，包括两条 --+--占位
     result = printGroup(rd.table_data, rd.keyHeader, rd.keyMaxLen)
     return result
+
+def table_csvout(inf_ls:list[SingleSub]):
+    '''
+    Docstring for table_csvout
+    '''
+    c = RefineTable2csv(inf_ls)
+    c.get_original_df() 
+
+
+def run():
+    s = Subject()
+
+
+    s.inf_list_create()
+
+    s.deal_time_slot()
     
+    inf_ls = s.sort_inflist_for_table()
+    # df = s.convert_inflist_to_df(inf_ls)
+
+    # data = df.to_dict('records')
+
+    f = table_csvout(inf_ls)
+    
+    
+    # with open('temp_output.txt', 'w') as f:
+    #     f.writelines(a)    
 
 if __name__ == '__main__':
-    df = pd.read_csv('tests/base_files/out_table_2.csv')
-    # data = df.to_dict('records')
-    a = table_printout(df)
-    # with open('temp_output.txt', 'w') as f:
-    #     f.writelines(a)
+    run()
