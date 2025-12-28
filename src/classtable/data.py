1f8bd215c273194a6ee4a9ddb32a98f2b93a62e6
shunw <shunw.li@outlook.com>
2025-12-28 20:25:17 +0800

    Time deal (#4)
    * deal with time info in the data.py
    
    * update the time block cal, which changes into the time as 30 or 00
    
    * complete the blocking time part.
    
    * trim the blocking logic

--- src/classtable/data.py
+++ src/classtable/data.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator, model_validator, ValidationInfo
 from enum import Enum
 from typing import Optional
 import csv
+import pandas as pd
+from datetime import datetime, timedelta
 
 class ClassCategory(str, Enum):
     ballet = "ballet"
@@ -33,16 +35,63 @@ class LocationInfo(BaseModel):
     # https://docs.pydantic.dev/latest/concepts/alias/
     loc_close: str = Field(description="the nearest city center", alias="close")
     
+def refine_dt(timedata:datetime):
+    '''
+    Docstring for refine_dt
+    this is to make the time data to 30 or 00
+    
+    :param timedata: Description
+    :type timedata: datetime
+    '''
+    h = timedata.hour
+    m = timedata.minute
+    
+    try:
+        if m - 30 > 15: 
+            new = timedata.replace(hour=h+1, minute= 0)
+            
+            
+        elif (m - 30 <= 15) and (m - 30 > -15):
+            new = timedata.replace(minute = 30)
+            
+        elif m - 30 <= -15:
+            new = timedata.replace(minute = 0)
+    
+        return new
+    except ValueError as e:
+        raise ValueError(f'the input {timedata} is something wrong') from e
+    
     
 class ClassScheduleInfo(BaseModel):
-    start_time: str
+    start_time_str: str = Field(alias = 'start_time')
+    start_time_dt: Optional[datetime] = None
     class_id: int
     day: int
     loc_id: int
     preferred: bool
     
+    @model_validator(mode = 'after')
+    def parse_str_to_datatime(self):
+        # 从values数据中获取已验证的字符串字段值
+        t_str = self.start_time_str
+        if t_str:
+            # csv中的自定义格式
+            
+            custom_format = "%H:%M"
+            try: 
+                # 尝试解析字符串
+                temp_dt = datetime.strptime(t_str, custom_format)
+                self.start_time_dt = refine_dt(temp_dt)
+                # print (self.start_time_dt.hour, self.start_time_dt.minute)
+                
+            except ValueError as e:
+                
+                raise ValueError(f"时间字符串 '{t_str}' 与预期格式 '%H/%M' 不匹配。") from e
+        return self
+    
 class ClassTable(BaseModel):
-    start_time: str
+    start_time_str: str
+    start_time_dt: datetime
     day: int
     preferred: bool
     class_name: str
@@ -53,6 +102,40 @@ class ClassTable(BaseModel):
     location: str
     home_dis: float
     loc_close: str
+
+class BlockTable(BaseModel):
+    '''
+    Docstring for BlockTable
+        for the event blocked, which will impact the class chosen
+    '''    
+    start_time_str: str = Field(alias='start_time')
+    start_time_dt: Optional[datetime] = None
+    end_time_str: str = Field(alias= 'end_time')
+    end_time_dt: Optional[datetime] = None
+    loc: str
+    event: str
+    day: int
+    
+    @model_validator(mode = 'after')
+    def parse_str_to_datatime(self):
+        # 从values数据中获取已验证的字符串字段值
+        # t_str = self.start_time_str
+        if self.start_time_str or self.end_time_str:
+            # csv中的自定义格式
+            
+            custom_format = "%H:%M"
+            try: 
+                # 尝试解析字符串
+                self.start_time_dt = datetime.strptime(self.start_time_str, custom_format)
+                self.end_time_dt = datetime.strptime(self.end_time_str, custom_format)
+                # self.start_time_dt = refine_dt(temp_dt)
+                # print (self.start_time_dt.hour, self.start_time_dt.minute)
+                
+            except ValueError as e:
+                
+                raise ValueError(f"时间字符串 '{self.start_time_str} or {self.end_time_str}' 与预期格式 '%H/%M' 不匹配。") from e
+        return self
+    
     
     
 def load_class_infos(csv_file: str) -> list[ClassInfo]:
@@ -70,10 +153,37 @@ def load_class_schedule_infos(csv_file: str) -> list[ClassScheduleInfo]:
         reader = csv.DictReader(f)
         return [ClassScheduleInfo.model_validate(row) for row in reader]
 
-def data_combine() -> list:
+def load_block_schedule(csv_file: str) -> list[BlockTable]:
+    with open(csv_file, 'r', encoding= 'utf-8') as f:
+        reader = csv.DictReader(f)
+        return [BlockTable.model_validate(row) for row in reader]
+def be_blocked(cls_time:datetime, block_time_s:datetime, block_time_e:datetime) -> bool:
+    '''
+    block_time_s: start point of the block time
+    block_time_e: end point of the block time
+    cls_time: class time
+    return True means: yes, this time is blocked; 
+    return False means: no, this time is not blocked;
+    
+    先处理 block 时间的情况
+    不能上的情况：
+    如果课程的开始时间 在block的时间中间 
+    如果课程的开始时间 +1h 比 block的开始 时间晚，那就说明这个课程不能上
+    '''
+    # print (cls_time, block_time_s, block_time_e)
+    if (cls_time >= block_time_s) and (cls_time <= block_time_e):
+        return True
+    '''
+    此处逻辑有问题!!!
+    '''
+    if (cls_time <= block_time_s) and cls_time + timedelta(hours=1) >= block_time_s:
+        return True
+    return False
+    
+def data_combine() -> list[ClassTable]:
     '''
     Docstring for data_create
-    from three csv 
+    gather data from three csv / 将inf 组织到一起, 将几个table联系起来
         
     :return: Description
     :rtype: list
@@ -81,15 +191,52 @@ def data_combine() -> list:
     class_schedule = load_class_schedule_infos('db/class_schedule.csv')
     class_info = load_class_infos('db/class_inf.csv')
     loc_info = load_location_infos('db/loc_inf.csv')
+    final_schedule = list()
+    block_schedule = load_block_schedule('db/block_schedule.csv')    
+    
     for i in class_schedule:
-        
+        '''
+        先处理 block 时间的情况
+        不能上的情况：
+        如果课程的开始时间 在block的时间中间 
+        如果课程的开始时间 +1h 比 block的开始 时间晚，那就说明这个课程不能上
+        '''
+        flag = False # 没有block的时间
+        block_ls = list(filter(lambda p: p.day == i.day, block_schedule)) # 做成list 是因为觉得可能 一天里可能会有两个block的时间
+        # print (block_ls)
+        for b in block_ls:
+            flag = be_blocked(i.start_time_dt, b.start_time_dt, b.end_time_dt)
+            # print (flag)
+            if flag:
+                break
+        if flag:
+            continue
+                
+            
         cc = next(filter(lambda p: p.class_id == i.class_id, class_info), None)
         ll = next(filter(lambda p: p.loc_id == i.loc_id, loc_info), None)
         d = {**i.model_dump(), **cc.model_dump(), **ll.model_dump()}
 
-        cd = ClassTable.model_validate(d)
-        print (cd)
-        # break
+        final_schedule.append(ClassTable.model_validate(d))
+    
+    
+    
+    return final_schedule
+
+def run():
+    # class_schedule = load_block_schedule('db/block_schedule.csv')
+    # dict_ls = [d.model_dump() for d in class_schedule]
+    # print (pd.DataFrame(dict_ls))
+    
+    data_to_df()
+    
+def data_to_df() -> pd.DataFrame:
+    data_ls = data_combine()
+    dict_ls = [d.model_dump() for d in data_ls]
+    df = pd.DataFrame(dict_ls)
+    print (df)
+    
+    
 
 if __name__ == "__main__":
     # import argparse, os, sys
@@ -109,5 +256,4 @@ if __name__ == "__main__":
     # elif type == "location":
     #     load_func = load_location_infos
     # print("sub data: ", [item.model_dump() for item in load_func(csv_file)])
-    
-    data_combine()
+    run()
--- src/classtable/data_deal.py
+++ src/classtable/data_deal.py
@@ -1,62 +1,13 @@
 import pandas as pd
 import time
 from datetime import datetime
-
-# class SingleSub:
-#     def __init__(self, one_s:pd.Series):
-#         self.start_time = one_s.start_time
-#         self.class_name = one_s.class_name
-#         self.class_loc = one_s.location
-#         self.class_category = one_s.category
-#         self.class_dis = one_s.home_dis
-#         self.class_priority = one_s.priority
-#         self.day = one_s.day
-#         self.preferred = one_s.preferred
-#         self.period_lim = one_s.period_limit
-
-#         self.start_time_r = None
-#         self.selected = False
-    
-#     def dict_form(self):
-#         return (self.__dict__)
-
-#     def __str__(self):
-#         # return f'class:{self.class_name}; day:{self.day}; start_time:{self.start_time}; loc:{self.class_loc}; category:{self.class_category}'
-#         return f'{self.__dict__}'
-
+from classtable.data import data_combine
 
 
 class Subject:
     def __init__(self):
-        pass
+        self.inf_ls = data_combine()
     
-    def inf_list_create(self):
-        
-        loc_df = pd.read_csv('db/loc_inf.csv')
-        class_inf_df = pd.read_csv('db/class_inf.csv')
-        class_all_df = pd.read_csv('db/class_all.csv')
-        df = pd.merge(class_all_df,class_inf_df, on = 'class_id', how = 'left')
-        self.df = pd.merge(df, loc_df, on = 'loc_id', how = 'left')
-
-        self.inf_ls = list()
-        for ind in self.df.index:
-            item = SingleSub(self.df.loc[ind])
-            self.inf_ls.append(item) 
-            
-        return self.inf_ls
-    
-    def _refine_new_time(self,time_str:str):
-        h, m = time_str.split(':')
-        if int(m) - 30 > 15: 
-            return f'{int(h)+1}:00'
-        elif (int(m) - 30 <= 15) and (int(m) - 30 > -15):
-            return f'{int(h)}:30'
-        elif int(m) - 30 <= -15:
-            return f'{int(h)}:00'
-        else:
-            return 'something wrong'
-        
-
     def deal_time_slot(self):
         '''
         this is for the information list, this is to make the time into the block, like (11:25,11:40) -> 11:30; (11:05, 10:50)-> 11:00; 
@@ -80,7 +31,14 @@ class Subject:
                 self.max_p = i.start_time_r
 
     def arrange_time_table(self):
-        
+        '''
+        Docstring for arrange_time_table
+            逻辑是：
+                1. preferred 
+                2. 当天安排 （跑步，或者是 散步，block 时间和 地点）
+                3. 优先级
+        :param self: Description
+        '''
         # collect the information of arranged class
         arg_dict = dict() # {group_key: [case1, case2, ... ], group_key1: [case5, case6, ...]}
         # group_key_fd = # (preferred, priority, category)
@@ -115,24 +73,18 @@ class Subject:
         self.inf_ls_new = sorted(self.inf_ls, key = lambda x: (x.start_time_r, x.day))
         return self.inf_ls_new
 
-    def convert_inflist_to_df(self, target_inflist:list[SingleSub]):
-        data = []
-        for i in target_inflist:
-            data.append(i.dict_form())
-        df = pd.DataFrame(data)
-        return df
+    
     
 
 def run():
     s = Subject()
 
-    s.inf_list_create()
-    s.deal_time_slot()
-    # s.arrange_time_table()
+    # s.deal_time_slot()
+    # # s.arrange_time_table()
     
-    inf_ls = s.sort_inflist_for_table()
-    df = s.convert_inflist_to_df(inf_ls)
-    print (df)
+    # inf_ls = s.sort_inflist_for_table()
+    # df = s.convert_inflist_to_df(inf_ls)
+    # print (df)
 
 if __name__ == '__main__':
     
